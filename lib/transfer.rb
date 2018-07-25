9b6dfacbc87be09469602d0718c8d1f3536f5012
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-25 00:30:39 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,10 +10,10 @@ class Transfer
    sender.valid? && receiver.valid?
  end
  def execute_transaction
-   if valid? && sender.balance > 0 && @status=="open"
-     sender.balance-=amount
-     receiver.balance+=amount
-     self.status="complete"
+   if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
   end
  end
 end
