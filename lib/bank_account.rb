a0ba376d42eaf19d3ed6d035451f72ddfde4c84b
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-25 00:18:30 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,7 +13,7 @@ def display_balance
   "Your balance is $#{@balance}."
 end
 def valid?
-  if @status=="open" && @balace > 0
+  if @status=="open" && @balance > 0
     return true
   else return false 
 end
