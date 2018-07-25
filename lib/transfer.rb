931985c60f435b514f347fb33c77842f07c0ddf1
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-25 00:23:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
  attr_accessor :sender, :receiver, :status
- def initialize(sender, reciever)
+ def initialize(sender, receiver)
    @sender=sender
    @receiver=receiver
    @status="pending"
